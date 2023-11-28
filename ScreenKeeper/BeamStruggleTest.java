b11c976fc3b58e532fbfe619610f8047a9468244
FhebReker <146902117+FhebReker@users.noreply.github.com>
2023-11-28 14:20:57 -0500

    Update BeamStruggleTest.java

--- ScreenKeeper/BeamStruggleTest.java
+++ ScreenKeeper/BeamStruggleTest.java
@@ -5,20 +5,20 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class BeamStruggleTest extends JFrame {
-	private int cannonWidth = 150;
+	private int cannonWidth = 100;
 	private int cannonHeight = 100;	
 	
     private int cannon1X = 50; // X position of character 1    
     private int[] cannon1XBase = {cannon1X, cannon1X+50, cannon1X+100};
     private int[] cannon1YBase = {425, 350, 425};
     private Polygon cannon1Base = new Polygon(cannon1XBase, cannon1YBase, 3); 
-    private Rectangle beam1 = new Rectangle(cannon1X + 100, 340, 50, 20); // Beam 1 rectangle
+    private Rectangle beam1 = new Rectangle(cannon1X + 75, 340, 50, 20); // Beam 1 rectangle
    
     private int cannon2X = 650; // X position of character 2
     private int[] cannon2XBase = {cannon2X, cannon2X+50, cannon2X+100};
     private int[] cannon2YBase = {425, 350, 425};
     private Polygon cannon2 = new Polygon(cannon2XBase, cannon2YBase, 3); 
-    private Rectangle beam2 = new Rectangle(cannon2X - 50, 340, 50, 20); // Beam 2 rectangle
+    private Rectangle beam2 = new Rectangle(cannon2X - 25, 340, 50, 20); // Beam 2 rectangle
 
     private boolean beamsMeeting = false; // Flag to indicate if beams have met
 
@@ -42,15 +42,15 @@ public class BeamStruggleTest extends JFrame {
                 if (e.getKeyCode() == KeyEvent.VK_SPACE) {
                     if (!beamsMeeting) {
                         // Move the beams towards each other
-                        beam2.x -= 8;
+                        beam2.x -= 16;
 
                         // Update the width of the beams as they move
-                        beam1.width += 8;
-                        beam2.width += 8;
+                        beam1.width += 16;
+                        beam2.width += 16;
                         
                         // increasing height of beams to show power
-                        beam1.height+=1;
-                        beam2.height+=1;
+                        beam1.height+=2;
+                        beam2.height+=2;
                         beam1.y-=1;
                         beam2.y-=1;
 
@@ -70,6 +70,13 @@ public class BeamStruggleTest extends JFrame {
     public void paint(Graphics g) {
         super.paint(g);
 
+        // Draw beam 1
+        g.setColor(Color.ORANGE);
+        g.fillRect(beam1.x, beam1.y, beam1.width, beam1.height);
+
+        // Draw beam 2
+        g.fillRect(beam2.x, beam2.y, beam2.width, beam2.height);
+        
         // Draw character 1
         g.setColor(Color.RED);
         g.fillOval(cannon1X, 300, cannonWidth, cannonHeight);
@@ -81,13 +88,6 @@ public class BeamStruggleTest extends JFrame {
         g.fillOval(cannon2X, 300, cannonWidth, cannonHeight);
         g.setColor(Color.ORANGE);
         g.fillPolygon(cannon2);
-
-        // Draw beam 1
-        g.setColor(Color.ORANGE);
-        g.fillRect(beam1.x, beam1.y, beam1.width, beam1.height);
-
-        // Draw beam 2
-        g.fillRect(beam2.x, beam2.y, beam2.width, beam2.height);
         
         if (beamsMeeting) {
         	g.setColor(Color.CYAN);
