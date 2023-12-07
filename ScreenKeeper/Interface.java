723117967776305d9f97cc05b2caa1be5c363cc0
FhebReker <146902117+FhebReker@users.noreply.github.com>
2023-12-06 19:34:20 -0500

    Update Interface.java

--- ScreenKeeper/Interface.java
+++ ScreenKeeper/Interface.java
@@ -8,9 +8,7 @@ public class Interface{
 	static final int SLEEPTIME = 100;
 	static final int WindowWidth = 1200;
 	static final int WindowHeight = 800;
-	static final int changeTime = 25000;
 	
-	/***** Global (instance) Variables ******/
 	private static boolean keepPlaying = true;
 	private static GraphicsConsole gc = new GraphicsConsole (WindowWidth, WindowWidth);
 	
@@ -21,42 +19,69 @@ public class Interface{
 		gc.clear();
 		gc.drawString("Welcome to The Screensaver Experience", WindowWidth / 20, WindowHeight/2-350);
 		gc.drawString("There are 3 screensavers in total:", WindowWidth/20, (WindowHeight / 2) - 300);
-		gc.drawString("Mandalas, Cannon Beam Blast, INSERT WHATEVER THE THIS ONE IS HERE", WindowWidth/20, (WindowHeight / 2) - 250);
+		gc.drawString("Flower Mandalas, Cannon Beam Blast, Ball Accelerator", WindowWidth/20, (WindowHeight / 2) - 250);
 		gc.drawString("Hope you enjoy this presentation!", WindowWidth/20, (WindowHeight / 2));
 		gc.drawString("Starting in 5 seconds...", WindowWidth/20, (WindowHeight) - 300);
 		gc.sleep(5000);
+	} 
+	public static void main (String[] args) {
+		startScreen();
+		int looperIndex = 0;
+		while(keepPlaying) {
+			switch(looperIndex) {
+				case 0:
+					Mandala.mandalaMaker(gc);
+					break;
+				case 1:
+					BeamStruggle.beamStruggle(gc);
+					break;
+				case 2:
+					BallAccelerator.Accelerate(gc);
+					break;
+				case 3:
+					looperIndex = -1;	break;
+			}
+			looperIndex++;
+			gc.sleep(1000);
+			gc.clear();
+		}
 	}
+}
+
+class Mandala{
+	static final int SLEEPTIME = 100;
+	static final int WindowWidth = 1200;
+	static final int WindowHeight = 800;
 	
-	// Mandala Maker
-	public static void mandala() {		
+	public static void mandalaMaker(GraphicsConsole gc) {		
 		gc.clear();
 		int x,y, count = 3;
 		
 		y = WindowHeight/2;
 		x = WindowWidth/4; 
 		for(int i = 0; i<5; i++) {
-			floralDraw(x*i, WindowHeight, count, Color.magenta);
-			floralDraw(x*i, y, count, Color.magenta);
-			floralDraw(x*i, 0, count, Color.magenta);
+			floralDraw(x*i, WindowHeight, count, Color.magenta, gc);
+			floralDraw(x*i, y, count, Color.magenta, gc);
+			floralDraw(x*i, 0, count, Color.magenta, gc);
 		}
 		
 		count+= 3;
 		y = WindowHeight/4;
 		x = WindowWidth/3; 
 		for(int i = 0; i<4; i++) {
-			floralDraw(x*i, y, count, Color.ORANGE);
-			floralDraw(x*i, 3*y, count, Color.ORANGE);
+			floralDraw(x*i, y, count, Color.ORANGE, gc);
+			floralDraw(x*i, 3*y, count, Color.ORANGE, gc);
 		}
 		
 		count+= 5;
 		y = WindowHeight/2;
 		x = WindowWidth/6; 
-		floralDraw(x, y, count, Color.red);
-		floralDraw(x*5, y, count, Color.red);
+		floralDraw(x, y, count, Color.red, gc);
+		floralDraw(x*5, y, count, Color.red, gc);
 
 		count+= 2;
 		x = WindowWidth/2; 
-		floralDraw(x, y, count, Color.GREEN);
+		floralDraw(x, y, count, Color.GREEN, gc);
 	}
 	private static Color colorChange(Color previousColor, int randomIncrease) {
 		Color newColor;
@@ -76,7 +101,7 @@ public class Interface{
 		}
 		return newColor;
 	}
-	private static void floralDraw(int centerX, int centerY, int numCircles, Color firstRingColor) {
+	private static void floralDraw(int centerX, int centerY, int numCircles, Color firstRingColor, GraphicsConsole gc) {
 		int x, y, 
 		radius = 10 + 20*numCircles,
 		numLines = 12,
@@ -115,6 +140,12 @@ public class Interface{
     		innerCircleColor = colorChange(innerCircleColor, randomColorChanger);
         }
 	}
+}
+
+class BeamStruggle{
+	
+	static final int WindowWidth = 1200;
+	static final int WindowHeight = 800;
 	
 	// Beam Struggle
 	private static int cannonWidth = 150;
@@ -134,7 +165,7 @@ public class Interface{
     private static Polygon cannon2Base = new Polygon(cannon2XBase, cannon2YBase, 3); 
     private static Rectangle beam2 = new Rectangle(cannon2X - 25, 365, 50, 20);
 	
-    public static void beamStruggle() {		   		 
+    public static void beamStruggle(GraphicsConsole gc) {		   		 
 		 gc.setBackgroundColor(Color.black);
 		 gc.clear();
 		 resetBeams();
@@ -195,10 +226,10 @@ public class Interface{
 			 }
 		
 			 if (beam1.height <= 0 || beam2.height <= 0)	break;
-    		beamDraw();
+    		beamDraw(gc);
 		 }
 	}
-    private static void beamDraw() {
+    private static void beamDraw(GraphicsConsole gc) {
 		 // Draw beam 1
 		 gc.setColor(Color.MAGENTA);
 		 gc.fillRect(beam1.x, beam1.y, beam1.width, beam1.height);
@@ -248,25 +279,95 @@ public class Interface{
         beam2 = new Rectangle(cannon2X - 25, 365, 50, 20); // Beam 2 rectangle
         beamsMeeting = false; // Flag to indicate if beams have met 
     }
+}
+
+class BallAccelerator{
+	static final int WindowWidth = 1200;
+	static final int WindowHeight = 800;
 	
-    
-    
-	public static void main (String[] args) {
-		//startScreen();
-		int looperIndex = 0;
-		while(keepPlaying) {
-			switch(looperIndex) {
-				case 0:
-					//mandala();		break;
-				case 1:
-					//beamStruggle();	break;
-				case 2:
-				case 3:
-					looperIndex = -1;	break;
-			}
-			looperIndex++;
-			gc.sleep(1000);
-			gc.clear();
+	private static int ballX, ballY; // Initial x and y position of the ball
+	private static int dx, dy; // Velocity of the logo
+	private static int ballRadius; // Width of the logo
+	private static int speedIncrease;
+	private static int backgroundRGB;
+	
+	private static Color[] colours = {new Color(0, 89, 83), new Color(228, 116, 32), new Color(90, 64, 111), new Color(110, 230, 167), new Color(110, 106, 167)};
+	private static int currentColor = 0;
+	
+	private static Line[] lines;
+	private static int lineIndex; // Index to track the next available position in the array
+	    
+	public static void Accelerate(GraphicsConsole gc) {
+		ballX = WindowWidth / 2 - 200; // Initial x position of the ball
+		ballY = WindowHeight / 2 - 200; // Initial y position of the ball
+		ballRadius = 225; // Width of the logo
+		backgroundRGB = 225;
+		currentColor = 0;
+		lines = new Line[200];
+		lineIndex = 0; // Index to track the next available position in the array
+		 
+		gc.setBackgroundColor(new Color(backgroundRGB, backgroundRGB, backgroundRGB));
+		dx = dy = 1;
+		speedIncrease = 5;
+		while (true) {
+			ballX += dx * speedIncrease;
+			ballY += dy * speedIncrease;
+			
+	        if (ballX <= 0)	
+	        	collision(ballX, ballY, ballX, ballY + (ballRadius/2), colours[currentColor], true, gc); // Create a vertical line at the collision	      
+	        else if (ballX >= WindowWidth - ballRadius)	
+	        	collision(ballX, ballY, ballX + ballRadius, ballY + (ballRadius/2), colours[currentColor], true, gc); // Create a vertical line at the collision	       
+	        
+	        if (ballY <= 0)	
+	        	collision(ballX, ballY, ballX + (ballRadius/2), ballY, colours[currentColor], false, gc); // Create a horizontal line at the collision	       
+	        else if (ballY >= WindowHeight - ballRadius)	
+	        	collision(ballX, ballY, ballX + (ballRadius / 2), ballY + ballRadius, colours[currentColor], false, gc); // Create a horizontal line at the collision
+	                
+	        drawer(gc);
+	        gc.sleep(50);	
+	        
+	    	if(lines[lines.length - 1] != null) {
+	    		gc.setBackgroundColor(Color.black);
+	    		gc.clear();
+	            break;
+	        }
 		}
+	} 
+	private static void drawer(GraphicsConsole gc) {
+		gc.clear();
+	    for (int i = 0; i < lines.length; i++) {        	
+	        if (lines[i] != null) {
+	        	lines[i].x1 = ballX +(ballRadius/2);
+	        	lines[i].y1 = ballY + (ballRadius/2);
+	            gc.setColor(lines[i].color);
+	            gc.drawLine(lines[i].x1, lines[i].y1, lines[i].x2, lines[i].y2);
+	        }
+	    }
+	    gc.setColor(colours[currentColor]);
+	    gc.fillOval(ballX, ballY, ballRadius, ballRadius);
 	}
+	private static void collision(int x1, int y1, int x2, int y2, Color color, boolean heightOrWidthBounding, GraphicsConsole gc) {
+		if (heightOrWidthBounding)	dx = -dx;
+		else dy = -dy;
+		speedIncrease++;
+	    currentColor = (currentColor + 1) % colours.length;
+	    ballRadius--;
+	    backgroundRGB--;
+		gc.setBackgroundColor(new Color(backgroundRGB, backgroundRGB, backgroundRGB));
+		
+	    lines[lineIndex] = new Line(x1, y1, x2, y2, color);
+	    lineIndex = (lineIndex + 1) % lines.length; // Move to the next position, wrapping around if necessary
+	}
+}
+
+class Line {
+int x1, y1, x2, y2;
+Color color;
+public Line(int x1, int y1, int x2, int y2, Color color) {
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+    this.color = color;
+}
 }
